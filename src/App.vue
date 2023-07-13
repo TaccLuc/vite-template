5c02b874b8286bdd9badf34df6cccfc817075f11
TaccLuc <tacc.luca@gmail.com>
2023-07-13 16:05:30 +0200

    Initialization

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+
+import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
+import FooterComp from './components/FooterComp.vue';
+
+export default{
+  components:{
+    HeaderComp,
+    MainComp,
+    FooterComp
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <HeaderComp />
+  <MainComp />
+  <FooterComp />
+
+</template>
+
+<style lang="scss">
+
+</style>
