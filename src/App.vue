550ce048d9333387cc719c96700eb21f2e0c773c
TaccLuc <tacc.luca@gmail.com>
2023-07-17 19:00:15 +0200

    Fix: store import

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import HeaderComp from './components/HeaderComp.vue';
 import MainComp from './components/MainComp.vue';
 import FooterComp from './components/FooterComp.vue';
-import store from './store';
+import {store} from './store';
 
 export default{
   components:{
